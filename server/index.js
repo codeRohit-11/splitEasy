e32d452668862e873c3dee5edd34c0892b9f8b55
codeRohit-11 <rohitchampion11@gmail.com>
2026-07-16 01:16:19 +0530

    Connecting mongoose to MongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect to MongoDB
+connectDB();
 
 // Enable CORS from the start
 app.use(cors());
